3b9ae7d1e2ac316a313c0ba675db6d3f33445b4b
Mathieu de Ruiter <mathieu@fellicht.nl>
2016-01-03 22:26:36 +0100

    Save user to localStorage.

--- src/js/app.js
+++ src/js/app.js
@@ -1,253 +1,306 @@
 var app = {
-        resources: [
-            'img/controller/button-a.png',
-            'img/controller/button-b.png',
-            'img/controller/button-x.png',
-            'img/controller/button-y.png',
-            'img/controller/button-down.png',
-            'img/controller/button-left.png',
-            'img/controller/button-right.png',
-            'img/controller/button-up.png',
-            'img/controller/thumbstick.png',
-            'img/controller/thumbstick-bg.png'
-        ],
-        gameConnected: false,
+    resources: [
+        'img/controller/button-a.png',
+        'img/controller/button-b.png',
+        'img/controller/button-x.png',
+        'img/controller/button-y.png',
+        'img/controller/button-down.png',
+        'img/controller/button-left.png',
+        'img/controller/button-right.png',
+        'img/controller/button-up.png',
+        'img/controller/thumbstick.png',
+        'img/controller/thumbstick-bg.png'
+    ],
+    gameConnected: false,
+    /**
+     * Global app settings
+     */
+    settings: {
         /**
-         * Global app settings
+         * @param int The width and height of the window/app. Will be changed on
+         * window resize.
          */
-        settings: {
-            /**
-             * @param int The width and height of the window/app. Will be changed on
-             * window resize.
-             */
-            width: window.innerWidth,
-            height: window.innerHeight,
-            gameCode: '',
-            host: 'http://localhost/couchfriends/api/',
-            user: {
-                token: null,
-                name: '',
-                color: 0xffffff
-            }
-        },
-        /**
-         * PIXI stage
-         */
-        stage: {},
-        /**
-         * PIXI renderer
-         */
-        renderer: {},
-        /**
-         * List with all buttons and ui that can be animated
-         */
-        elements: [],
-        // Application Constructor
-        initialize: function () {
-            this.bindEvents();
-        },
-        // Bind Event Listeners
-        //
-        // Bind any events that are required on startup. Common events are:
-        // 'load', 'deviceready', 'offline', and 'online'.
-        bindEvents: function () {
-            document.addEventListener('deviceready', app.startup, false);
-            window.addEventListener('load', app.startup, false);
-            window.addEventListener('resize', app.resize, false);
-        },
-        // deviceready Event Handler
-        //
-        // The scope of 'this' is the event. In order to call the 'receivedEvent'
-        // function, we must explicitly call 'app.receivedEvent(...);'
-        startup: function () {
-            app.receivedEvent('deviceready');
+        width: window.innerWidth,
+        height: window.innerHeight,
+        gameCode: '',
+        host: 'http://localhost/couchfriends/api/',
+        user: {
+            token: null,
+            name: '',
+            color: 0xffffff
+        }
+    },
+    /**
+     * PIXI stage
+     */
+    stage: {},
+    /**
+     * PIXI renderer
+     */
+    renderer: {},
+    /**
+     * List with all buttons and ui that can be animated
+     */
+    elements: [],
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function () {
+        document.addEventListener('deviceready', app.startup, false);
+        window.addEventListener('load', app.startup, false);
+        window.addEventListener('resize', app.resize, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    startup: function () {
+        app.receivedEvent('deviceready');
 
-            var renderer = PIXI.autoDetectRenderer(
-                app.settings.width,
-                app.settings.height,
-                {
-                    transparent: true
-                }
-            );
-            renderer.view.id = 'controller';
-            renderer.view.style.display = 'none';
-            document.body.appendChild(renderer.view);
-            app.renderer = renderer;
-            var stage = new PIXI.Container();
-            app.stage = stage;
-
-            var resources = app.resources;
-            for (var i = 0; i < resources.length; i++) {
-                var resource = resources[i];
-                PIXI.loader.add(resource);
+        var renderer = PIXI.autoDetectRenderer(
+            app.settings.width,
+            app.settings.height,
+            {
+                transparent: true
             }
-            PIXI.loader.on('complete', app.start);
-            PIXI.loader.load();
-        },
-        start: function () {
-            app.receivedEvent('start');
-            app.UI.init();
-            COUCHFRIENDS.on('connect', function () {
-                document.getElementById('controller').className = 'animated fadeOut';
-                app.UI.showForm();
-                document.getElementById('status').innerHTML = 'Connected';
-            });
-            COUCHFRIENDS.on('disconnect', function () {
-                document.getElementById('controller').className = 'animated fadeOut';
-                app.UI.showForm();
-                document.getElementById('status').innerHTML = 'Connected';
-                app.gameConnected = false;
-            });
-            COUCHFRIENDS.on('gameStart', function () {
-                document.getElementById('controller').style.display = 'block';
-                document.getElementById('controller').className = 'animated fadeIn';
-                app.gameConnected = true;
-                app.UI.hideMenu();
-                app.UI.showForm('join');
-            });
-            COUCHFRIENDS.on('gameDisconnect', function () {
-                document.getElementById('controller').style.display = 'none';
-                document.getElementById('controller').className = 'animated fadeOut';
-                app.gameConnected = false;
-                app.UI.showForm('join');
-            });
-            COUCHFRIENDS.on('playerIdentify', function (data) {
-                app.identify(data);
-            });
-            app.render();
+        );
+        renderer.view.id = 'controller';
+        renderer.view.style.display = 'none';
+        document.body.appendChild(renderer.view);
+        app.renderer = renderer;
+        var stage = new PIXI.Container();
+        app.stage = stage;
+
+        var resources = app.resources;
+        for (var i = 0; i < resources.length; i++) {
+            var resource = resources[i];
+            PIXI.loader.add(resource);
+        }
+        PIXI.loader.on('complete', app.start);
+        PIXI.loader.load();
+
+        try {
+            var user = window.localStorage.getItem('user');
+            if (user) {
+                app.setUser(JSON.parse(user));
+            }
+        }
+        catch (e) {
+
+        }
+    },
+    start: function () {
+        app.receivedEvent('start');
+        app.UI.init();
+        COUCHFRIENDS.on('connect', function () {
+            document.getElementById('controller').className = 'animated fadeOut';
+            app.UI.showForm();
+            document.getElementById('status').innerHTML = 'Connected';
+        });
+        COUCHFRIENDS.on('disconnect', function () {
+            document.getElementById('controller').className = 'animated fadeOut';
+            app.UI.showForm();
+            document.getElementById('status').innerHTML = 'Connected';
+            app.gameConnected = false;
+        });
+        COUCHFRIENDS.on('gameStart', function () {
+            document.getElementById('controller').style.display = 'block';
+            document.getElementById('controller').className = 'animated fadeIn';
+            app.gameConnected = true;
+            app.UI.hideMenu();
+            app.UI.showForm('join');
+        });
+        COUCHFRIENDS.on('gameDisconnect', function () {
+            document.getElementById('controller').style.display = 'none';
+            document.getElementById('controller').className = 'animated fadeOut';
+            app.gameConnected = false;
+            app.UI.showForm('join');
+        });
+        COUCHFRIENDS.on('playerIdentify', function (data) {
+            app.identify(data);
+        });
+        app.render();
+        COUCHFRIENDS.connect();
+        Controller.addButtonsABXY();
+        Controller.addAxis();
+    },
+    joinGame: function (code) {
+        code = code || document.getElementById('input-code').value;
+        if (code && code != '') {
+            app.settings.gameCode = code.toUpperCase();
+        }
+        if (!COUCHFRIENDS.connected) {
             COUCHFRIENDS.connect();
-            Controller.addButtonsABXY();
-            Controller.addAxis();
-        },
-        joinGame: function (code) {
-            code = code || document.getElementById('input-code').value;
-            if (code && code != '') {
-                app.settings.gameCode = code.toUpperCase();
+            COUCHFRIENDS.emit('error', {message: 'Not connected, check your internet and try again.'});
+            return false;
+        }
+        COUCHFRIENDS.send({
+            topic: 'player',
+            action: 'join',
+            data: {
+                code: app.settings.gameCode
             }
-            if (!COUCHFRIENDS.connected) {
-                COUCHFRIENDS.connect();
-                COUCHFRIENDS.emit('error', {message: 'Not connected, check your internet and try again.'});
-                return false;
-            }
-            COUCHFRIENDS.send({
-                topic: 'player',
-                action: 'join',
-                data: {
-                    code: app.settings.gameCode
-                }
-            });
-        },
-        // Update DOM on a Received Event
-        receivedEvent: function (id) {
-            //console.log('received', id);
-        },
+        });
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+        //console.log('received', id);
+    },
 
-        /**
-         * Window is resized or the screen is rotated. Reposition the buttons and
-         * fill the canvas to 100%
-         */
-        resize: function () {
-            var controllerCanvas = document.getElementById('controller');
-            if (controllerCanvas == null) {
-                return false;
-            }
-            var w = window.innerWidth;
-            var h = window.innerHeight;
-            app.settings.width = w;
-            app.settings.height = h;
-            app.renderer.resize(w, h);
+    /**
+     * Window is resized or the screen is rotated. Reposition the buttons and
+     * fill the canvas to 100%
+     */
+    resize: function () {
+        var controllerCanvas = document.getElementById('controller');
+        if (controllerCanvas == null) {
+            return false;
+        }
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        app.settings.width = w;
+        app.settings.height = h;
+        app.renderer.resize(w, h);
 
+        for (var i = 0; i < app.elements.length; i++) {
+            var element = app.elements[i];
+            element.resize();
+        }
+
+        return true;
+    },
+
+    /**
+     * Player identifier
+     * @param data
+     */
+    identify: function (data) {
+
+        data = data || {};
+        if (data.color != null) {
             for (var i = 0; i < app.elements.length; i++) {
                 var element = app.elements[i];
-                element.resize();
+                element.setTint(data.color);
             }
+        }
+    },
 
-            return true;
-        },
+    render: function () {
+        requestAnimationFrame(app.render);
+        if (!COUCHFRIENDS.connected) {
+            return;
+        }
+        app.renderer.render(app.stage);
+    },
 
-        /**
-         * Player identifier
-         * @param data
-         */
-        identify: function (data) {
-
-            data = data || {};
-            if (data.color != null) {
-                for (var i = 0; i < app.elements.length; i++) {
-                    var element = app.elements[i];
-                    element.setTint(data.color);
+    login: function () {
+        var email = document.getElementById('login-email').value;
+        var password = document.getElementById('login-password').value;
+        var urlLogin = app.settings.host;
+        urlLogin += 'users/token';
+        var data = {
+            "email": email,
+            "password": password
+        };
+        var headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        };
+        ajax(urlLogin, JSON.stringify(data), function (data) {
+                data = JSON.parse(data);
+                if (!data.success) {
+                    return app.error(data.data.message);
                 }
-            }
-        },
-
-        render: function () {
-            requestAnimationFrame(app.render);
-            if (!COUCHFRIENDS.connected) {
-                return;
-            }
-            app.renderer.render(app.stage);
-        },
-
-        login: function () {
-            var email = document.getElementById('login-email').value;
-            var password = document.getElementById('login-password').value;
-            var urlLogin = app.settings.host;
-            urlLogin += 'users/token';
-            var data = {
-                "email": email,
-                "password": password
-            };
-            var headers = {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            };
-            ajax(urlLogin, JSON.stringify(data), function (data) {
-                    data = JSON.parse(data);
-                    if (!data.success) {
-                        return app.error(data.data.message);
-                    }
-                    app.UI.showForm('join');
-                    app.settings.user = data.data;
-                },
-                headers);
-        },
-        register: function () {
-            var name = document.getElementById('register-name').value;
-            var email = document.getElementById('register-email').value;
-            var password = document.getElementById('register-password').value;
-            var urlRegister = app.settings.host;
-            urlRegister += 'users/add';
-            var data = {
-                "profile": {
-                    "name": name
-                },
-                "email": email,
-                "password": password
-            };
-            var headers = {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            };
-            ajax(urlRegister, JSON.stringify(data), function (data) {
-                    data = JSON.parse(data);
-                    if (!data.success) {
-                        return app.error(data.data.message);
-                    }
-                    app.UI.showForm('join');
-                    app.settings.user = data.data;
-                },
-                headers);
-        },
-        logout: function () {
-            app.UI.hideMenu();
-            app.settings.user.token = null;
+                app.UI.showForm('join');
+                var user = data.data;
+                app.saveUser(user);
+                app.setUser(user);
+            },
+            headers);
+    },
+    register: function () {
+        var name = document.getElementById('register-name').value;
+        var email = document.getElementById('register-email').value;
+        var password = document.getElementById('register-password').value;
+        var urlRegister = app.settings.host;
+        urlRegister += 'users/add';
+        var data = {
+            "profile": {
+                "name": name
+            },
+            "email": email,
+            "password": password
+        };
+        var headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        };
+        ajax(urlRegister, JSON.stringify(data), function (data) {
+                data = JSON.parse(data);
+                if (!data.success) {
+                    return app.error(data.data.message);
+                }
+                app.UI.showForm('join');
+                var user = data.data;
+                app.saveUser(user);
+                app.setUser(user);
+                app.settings.user = user;
+            },
+            headers);
+    },
+    logout: function () {
+        app.saveUser();
+        app.setUser();
+        app.UI.hideMenu();
+    },
+    /**
+     * Set the user in the app settings variable.
+     *
+     * @param [user] object An object with user information
+     * @param user.token string the string to communicate with the server.
+     * @param user.name string the name of the player
+     * @param user.color string|int the color of the user. Default 0xffffff
+     */
+    setUser: function(user) {
+        user = user || {};
+        app.settings.user = {
+            token: user.token || null,
+            name: user.name || '',
+            color: user.color || 0xffffff
+        };
+    },
+    /**
+     * Saves the user in localstorage.
+     *
+     * @param [user] object
+     * @param user.token string the string to communicate with the server.
+     * @param user.name string the name of the player
+     * @param user.color string|int the color of the user. Default 0xffffff
+     */
+    saveUser: function(user) {
+        user = user || {};
+        var saveUser = {
+            token: user.token || null,
+            name: user.name || '',
+            color: user.color || 0xffffff
+        };
+        try {
+            window.localStorage.setItem('user', JSON.stringify(saveUser));
         }
-        ,
-        error: function (message) {
-            //console.warn(message);
+        catch (e) {
+            app.error('Cannot save the user.');
         }
+
+    },
+    error: function (message) {
+        //console.warn(message);
     }
-    ;
+};
 app.UI = {
     formNames: [
         'login', 'register', 'join', 'help'
